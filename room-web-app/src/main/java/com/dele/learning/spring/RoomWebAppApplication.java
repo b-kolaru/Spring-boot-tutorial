9e94d531886911c942dad7af15a3a96ad84daba4
b-kolaru <b-kolaru@gmail.com>
2022-04-26 13:58:28 -0400

    added dockerfile

---
+++ room-web-app/src/main/java/com/dele/learning/spring/RoomWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.dele.learning.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomWebAppApplication.class, args);
+	}
+
+}
